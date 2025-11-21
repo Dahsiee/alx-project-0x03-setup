91dc56d9411358ed13398bbd02da37f7dbc74652
Dahisee <chuksgabriella2019@gmail.com>
2025-11-21 12:23:27 +0000

    ..

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import {LayoutProps} from "@/interfaces";
+import {LayoutProps} from "../../interfaces";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
